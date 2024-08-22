7a239f0fa3fd81be9b0b51e11b3a549b7a3aaf5c
Dawood Mehmood <dawoodmehmood52222@gmail.com>
2024-08-23 00:03:03 +0500

    changed proxy admin's update function modifier

--- contracts/vendor/proxy/transparent/ProxyAdmin.sol
+++ contracts/vendor/proxy/transparent/ProxyAdmin.sol
@@ -59,7 +59,7 @@ contract ProxyAdmin is Ownable {
      *
      * - This contract must be the admin of `proxy`.
      */
-    function upgrade(TransparentUpgradeableProxy proxy, address implementation) public virtual onlyOwner {
+    function upgrade(TransparentUpgradeableProxy proxy, address implementation) public virtual ownerAndMarketUpdateAdmin {
         proxy.upgradeTo(implementation);
     }
 

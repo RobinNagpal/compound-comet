cd64f6f160d54ccaaa4a5dd0b81a4be0d9749a65
Muhammad Sami Tariq <68412933+MSamiTariq@users.noreply.github.com>
2024-08-30 23:15:56 +0500

    Update proposer tests (#18)

--- test/marketupdates/market-update-proposer-test.ts
+++ test/marketupdates/market-update-proposer-test.ts
@@ -1,5 +1,5 @@
 import { makeMarketAdmin } from './market-updates-helper';
-import { expect, makeConfigurator } from '../helpers';
+import { expect, makeConfigurator, ethers, wait, event } from '../helpers';
 
 describe('MarketUpdateProposer', function() {
   // We are not checking market updates here. we are just checking interaction
@@ -50,24 +50,206 @@ describe('MarketUpdateProposer', function() {
       users: [alice],
     } = await makeConfigurator();
 
-    expect(await marketUpdateProposer.owner()).to.equal(marketUpdateMultiSig.address);
+    expect(await marketUpdateProposer.owner()).to.equal(
+      marketUpdateMultiSig.address
+    );
 
-    await marketUpdateProposer.connect(marketUpdateMultiSig).transferOwnership(alice.address);
+    await marketUpdateProposer
+      .connect(marketUpdateMultiSig)
+      .transferOwnership(alice.address);
 
     expect(await marketUpdateProposer.owner()).to.equal(alice.address);
   });
 
-  it('only allows MarketUpdateMultisig to create proposal', async () => {});
+  it('only allows MarketUpdateMultisig to create proposal', async () => {
+    const {
+      marketUpdateProposer,
+      marketUpdateMultiSig,
+    } = await makeMarketAdmin();
 
-  it('keeps track of all the proposals', async () => {});
+    const {
+      configuratorProxy,
+      cometProxy,
+      users: [alice],
+    } = await makeConfigurator();
 
-  it('keeps track of all the proposals', async () => {});
+    let setPauseGuardianCalldata = ethers.utils.defaultAbiCoder.encode(
+      ['address', 'address'],
+      [cometProxy.address, alice.address]
+    );
+
+    const proposalId = 1n;
+    const proposalDescription = 'Test Proposal';
+
+    // Creates a proposal successfully as the signer is the multisig
+    const txn = await wait(
+      marketUpdateProposer
+        .connect(marketUpdateMultiSig)
+        .propose(
+          [configuratorProxy.address],
+          [0],
+          ['setPauseGuardian(address,address)'],
+          [setPauseGuardianCalldata],
+          'Test Proposal'
+        )
+    );
+
+    // Checks the emitter event properly
+    expect(event(txn, 0)).to.be.deep.equal({
+      MarketUpdateProposalCreated: {
+        id: proposalId,
+        proposer: marketUpdateMultiSig.address,
+        targets: [configuratorProxy.address],
+        signatures: ['setPauseGuardian(address,address)'],
+        calldatas: [setPauseGuardianCalldata],
+        description: proposalDescription,
+      },
+    });
+
+    // this will fail because the signer is not the multisig
+    await expect(
+      marketUpdateProposer
+        .connect(alice)
+        .propose(
+          [configuratorProxy.address],
+          [0],
+          ['setPauseGuardian(address,address)'],
+          [setPauseGuardianCalldata],
+          proposalDescription
+        )
+    ).to.be.revertedWith('Ownable: caller is not the owner');
+  });
+
+  it('keeps track of all the proposals', async () => {
+    const {
+      marketUpdateProposer,
+      marketUpdateMultiSig,
+    } = await makeMarketAdmin();
+
+    const {
+      configuratorProxy,
+      cometProxy,
+      users: [alice],
+    } = await makeConfigurator();
+
+    let setPauseGuardianCalldata = ethers.utils.defaultAbiCoder.encode(
+      ['address', 'address'],
+      [cometProxy.address, alice.address]
+    );
+
+    const proposalId = 1n;
+    const proposalDescription = 'Test Proposal';
+
+    // Creates a proposal successfully as the signer is the multisig
+    await marketUpdateProposer
+      .connect(marketUpdateMultiSig)
+      .propose(
+        [configuratorProxy.address],
+        [0],
+        ['setPauseGuardian(address,address)'],
+        [setPauseGuardianCalldata],
+        proposalDescription
+      );
+
+    // Checks the proposal
+    const proposal = await marketUpdateProposer.getProposal(proposalId);
+
+    expect(proposal.id).to.equal(proposalId);
+    expect(proposal.proposer).to.equal(marketUpdateMultiSig.address);
+    expect(proposal.targets[0]).to.equal(configuratorProxy.address);
+    expect(proposal.signatures[0]).to.equal(
+      'setPauseGuardian(address,address)'
+    );
+    expect(proposal.calldatas[0]).to.equal(setPauseGuardianCalldata);
+    expect(proposal.description).to.equal(proposalDescription);
+  });
 
   it('can cancel the proposal', async () => {
     // Create a proposal
     // Cancel the proposal
     // Check if the proposal is cancelled
+    const {
+      marketUpdateProposer,
+      marketUpdateMultiSig,
+    } = await makeMarketAdmin();
+
+    const {
+      configuratorProxy,
+      cometProxy,
+      users: [alice],
+    } = await makeConfigurator();
+
+    let setPauseGuardianCalldata = ethers.utils.defaultAbiCoder.encode(
+      ['address', 'address'],
+      [cometProxy.address, alice.address]
+    );
+
+    const proposalId = 1n;
+    const proposalDescription = 'Test Proposal';
+
+    // Creates a proposal successfully as the signer is the multisig
+    await marketUpdateProposer
+      .connect(marketUpdateMultiSig)
+      .propose(
+        [configuratorProxy.address],
+        [0],
+        ['setPauseGuardian(address,address)'],
+        [setPauseGuardianCalldata],
+        proposalDescription
+      );
+
+    expect(
+      (await marketUpdateProposer.proposals(proposalId)).canceled
+    ).to.be.equal(false);
+
+    // Cancel the proposal
+    await marketUpdateProposer.connect(marketUpdateMultiSig).cancel(proposalId);
+
+    expect(
+      (await marketUpdateProposer.proposals(proposalId)).canceled
+    ).to.be.equal(true);
+
+    // TODO: Try executing the cancelled proposal. It should fail
   });
 
-  it('marks the proposal as expired after grace period', () => {});
+  it('marks the proposal as expired after grace period', async () => {
+    const {
+      marketUpdateProposer,
+      marketUpdateMultiSig,
+    } = await makeMarketAdmin();
+
+    const {
+      configuratorProxy,
+      cometProxy,
+      users: [alice],
+    } = await makeConfigurator();
+
+    let setPauseGuardianCalldata = ethers.utils.defaultAbiCoder.encode(
+      ['address', 'address'],
+      [cometProxy.address, alice.address]
+    );
+
+    const proposalId = 1n;
+    const proposalDescription = 'Test Proposal';
+
+    // Creates a proposal successfully as the signer is the multisig
+    await marketUpdateProposer
+      .connect(marketUpdateMultiSig)
+      .propose(
+        [configuratorProxy.address],
+        [0],
+        ['setPauseGuardian(address,address)'],
+        [setPauseGuardianCalldata],
+        proposalDescription
+      );
+
+    // Fast forward time by more than the GRACE_PERIOD
+    const GRACE_PERIOD = 14 * 24 * 60 * 60; // 14 days in seconds
+    await ethers.provider.send('evm_increaseTime', [GRACE_PERIOD + 1]); // Increase by 14 days + 1 second
+    await ethers.provider.send('evm_mine', []); // Mine the next block to apply the time increase
+
+    expect(await marketUpdateProposer.state(proposalId)).to.equal(3); // Proposal should be expired
+
+    // TODO: Try executing the Expired proposal. It should fail
+  });
 });

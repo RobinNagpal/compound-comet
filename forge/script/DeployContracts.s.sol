474b82bfaaae18d896df9e8653cf602897c53bca
MSamiTariq <samitariq40@gmail.com>
2024-09-23 22:31:03 +0500

    improved the deployment script

--- forge/script/DeployContracts.s.sol
+++ forge/script/DeployContracts.s.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "forge-std/Script.sol";
+import "../lib/forge-std/src/Script.sol";
 import "../../contracts/Create2Deployer.sol";
-import "./contracts/MultiSigWallet.sol";
+import "./MarketUpdateConstants.sol";
 import "../../contracts/marketupdates/MarketUpdateTimelock.sol";
 import "../../contracts/marketupdates/MarketUpdateProposer.sol";
 import "../../contracts/Configurator.sol";
@@ -11,42 +11,34 @@ import "../../contracts/CometProxyAdmin.sol";
 
 contract DeployContracts is Script {
     address[] public owners; // Dynamic array to store owners
-    // Variable to store the deployed multisig wallet address
+    // Variable to store the deployed safe wallet address
     address public deployedWalletAddress;
 
     function run() external {
-        // Load environment variables (if using `.env`)
+        // Load environment variables
         string memory opSepoliaRpc = vm.envString("OP_SEPOLIA_RPC");
         string memory arbSepoliaRpc = vm.envString("ARB_SEPOLIA_RPC");
         string memory ethSepoliaRpc = vm.envString("ETH_SEPOLIA_RPC");
-        // string memory privateKey = vm.envString("PRIVATE_KEY");
 
-        // Set up deployer and RPC URL
         vm.startBroadcast(vm.envUint("PRIVATE_KEY"));
 
         // Set up the owners array
-        owners.push(0x7053e25f7076F4986D632A3C04313C81831e0d55);
-        owners.push(0x77B65c68E52C31eb844fb3b4864B91133e2C1308);
+        owners.push(MarketUpdateConstants.OWNER_1);
+        owners.push(MarketUpdateConstants.OWNER_2);
         owners.push(msg.sender); // You can change to a specific deployer address
 
-        uint256 threshold = 2; // Required number of approvals
         bytes32 salt = keccak256(abi.encodePacked("Salt-25"));
 
-        // Deploy MultiSigWallet
-        MultiSigWallet wallet = new MultiSigWallet(owners, threshold);
-        deployedWalletAddress = address(wallet); // Store the address in the variable
+        // Assuming that the Safe Wallet is already deployed
+        deployedWalletAddress = MarketUpdateConstants.WALLET_ADDRESS; // Store the address in the variable
 
         // Continue with other contract deployments using CREATE2Deployer
         Create2Deployer create2Deployer = new Create2Deployer();
 
-        // Prepare constructor arguments for MarketUpdateTimelock
-        address governorTimelockAddress = 0x6d903f6003cca6255D85CcA4D3B5E5146dC33925;
-        uint256 delay = 360000; // Set the desired delay (e.g., 3600 seconds)
-
         // Encode the constructor arguments into the bytecode
         bytes memory bytecode1 = abi.encodePacked(
             type(MarketUpdateTimelock).creationCode,
-            abi.encode(governorTimelockAddress, delay)
+            abi.encode(MarketUpdateConstants.GOVERNOR_TIMELOCK_ADDRESS, MarketUpdateConstants.DELAY)
         );
         // Precomputing the address of MarketUpdateTimelock contract
         address marketUpdateTimelockAddress = create2Deployer.computeAddress(salt, keccak256(bytecode1));
@@ -54,36 +46,34 @@ contract DeployContracts is Script {
 
         require(marketUpdateTimelockDeployedAddress == marketUpdateTimelockAddress, "Deployed address does not match the computed address");
 
-        // Prepare constructor arguments for MarketUpdateProposer
-        address pauseGuardianAddr = 0x7053e25f7076F4986D632A3C04313C81831e0d55;
         // Encode the constructor arguments into the bytecode
         bytes memory bytecode2 = abi.encodePacked(
             type(MarketUpdateProposer).creationCode,
-            abi.encode(governorTimelockAddress, deployedWalletAddress, pauseGuardianAddr, marketUpdateTimelockAddress)
+            abi.encode(MarketUpdateConstants.GOVERNOR_TIMELOCK_ADDRESS, deployedWalletAddress, MarketUpdateConstants.PAUSE_GUARDIAN_ADDRESS, marketUpdateTimelockAddress)
         );
         // Precompute the address of MarketUpdateProposer contract
-        address MarketUpdateProposerAddress = create2Deployer.computeAddress(salt, keccak256(bytecode2));
+        address marketUpdateProposerAddress = create2Deployer.computeAddress(salt, keccak256(bytecode2));
         address marketUpdateProposerDeployedAddress = create2Deployer.deploy(0, salt, bytecode2);
-        require(marketUpdateProposerDeployedAddress == MarketUpdateProposerAddress, "Deployed address does not match the computed address");
+        require(marketUpdateProposerDeployedAddress == marketUpdateProposerAddress, "Deployed address does not match the computed address");
 
         // Prepare bytecode for Configurator
         bytes memory bytecode3 = abi.encodePacked(
             type(Configurator).creationCode
         );
         // Precompute the address of Configurator contract
-        address ConfiguratorAddress = create2Deployer.computeAddress(salt, keccak256(bytecode3));
+        address configuratorAddress = create2Deployer.computeAddress(salt, keccak256(bytecode3));
 
-        address ConfiguratorDeployedAddress = create2Deployer.deploy(0, salt, bytecode3);
-        require(ConfiguratorDeployedAddress == ConfiguratorAddress, "Deployed address does not match the computed address");
+        address configuratorDeployedAddress = create2Deployer.deploy(0, salt, bytecode3);
+        require(configuratorDeployedAddress == configuratorAddress, "Deployed address does not match the computed address");
 
         // Prepare bytecode for CometProxyAdmin
         bytes memory bytecode4 = abi.encodePacked(
             type(CometProxyAdmin).creationCode
         );
         // Precompute the address of CometProxyAdmin contract
-        address CometProxyAdminAddress = create2Deployer.computeAddress(salt, keccak256(bytecode4));
-        address CometProxyAdminDeployedAddress = create2Deployer.deploy(0, salt, bytecode4);
-        require(CometProxyAdminDeployedAddress == CometProxyAdminAddress, "Deployed address does not match the computed address");
+        address cometProxyAdminAddress = create2Deployer.computeAddress(salt, keccak256(bytecode4));
+        address cometProxyAdminDeployedAddress = create2Deployer.deploy(0, salt, bytecode4);
+        require(cometProxyAdminDeployedAddress == cometProxyAdminAddress, "Deployed address does not match the computed address");
 
         vm.stopBroadcast();
     }
---
+++ forge/script/MarketUpdateConstants.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+library MarketUpdateConstants {
+    // Define your variables as constant or immutable values
+    address constant OWNER_1 = 0x7053e25f7076F4986D632A3C04313C81831e0d55;
+    address constant OWNER_2 = 0x77B65c68E52C31eb844fb3b4864B91133e2C1308;
+    address constant WALLET_ADDRESS = 0x6d903f6003cca6255D85CcA4D3B5E5146dC33925;
+    address constant GOVERNOR_TIMELOCK_ADDRESS = 0x6d903f6003cca6255D85CcA4D3B5E5146dC33925;
+    address constant PAUSE_GUARDIAN_ADDRESS = 0x7053e25f7076F4986D632A3C04313C81831e0d55;
+    uint256 constant DELAY = 360000;
+}

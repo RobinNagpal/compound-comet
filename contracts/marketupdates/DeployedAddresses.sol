c60c77020709b7a3be6adb14e0c84266476cf181
robin <robinnagpal.tiet@gmail.com>
2024-09-27 16:41:04 -0400

    Some cleanups

---
+++ contracts/marketupdates/DeployedAddresses.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.15;
+
+contract DeployedAddresses {
+    address public constant computedMarketUpdateTimelockAddress = 0x07b7e8dd3c97744842079262c8c92b04032a2c4a;
+    address public constant computedMarketUpdateProposerAddress = 0xbbb951875e87d7318fd6ab721b77b3e984576ccb;
+    address public constant computedConfiguratorAddress = 0x4618b5c7460911fb419fd6df4ed274ed64ef5b12;
+    address public constant computedCometProxyAdminAddress = 0x76aa25efdb6f995c9f3a7c5be6c9b225c4c2d084;
+    address public constant computedMarketAdminPermissionCheckerAddress = 0x4aa4c4a0e93ffafc4648696a542156bec5c3f2f0;
+}

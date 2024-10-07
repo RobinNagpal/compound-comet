e82a7eaf68e45bcdc01cfa39e592a8371bc54467
MSamiTariq <samitariq40@gmail.com>
2024-10-07 20:39:56 +0500

    added prepare block for scroll

--- deployments/scroll/usdc/migrations/1728038985_my_migration.ts
+++ deployments/scroll/usdc/migrations/1728038985_my_migration.ts
@@ -1,14 +1,70 @@
-import { DeploymentManager } from "../../../../plugins/deployment_manager/DeploymentManager";
-import { migration } from "../../../../plugins/deployment_manager/Migration";
-import { exp, proposal } from "../../../../src/deploy";
+import { DeploymentManager } from '../../../../plugins/deployment_manager/DeploymentManager';
+import { migration } from '../../../../plugins/deployment_manager/Migration';
+import { exp, proposal } from '../../../../src/deploy';
 
 interface Vars {
   deployedContracts: any;
 }
+const governorTimelockAddress = '0x6d903f6003cca6255D85CcA4D3B5E5146dC33925';
+const marketUpdateMultiSig = '0x7053e25f7076F4986D632A3C04313C81831e0d55';
+const marketUpdateProposalGuardian =
+  '0x77B65c68E52C31eb844fb3b4864B91133e2C1308';
+const delay = 360000;
 
-export default migration("1728038985_my_migration", {
+export default migration('1728038985_my_migration', {
   prepare: async (deploymentManager: DeploymentManager) => {
-    const deployedContracts = {};
+    const ethers = deploymentManager.hre.ethers;
+
+    const marketUpdateTimelock = await deploymentManager.deploy(
+      'marketUpdateTimelock',
+      'marketupdates/MarketUpdateTimelock.sol',
+      [governorTimelockAddress, delay],
+      true
+    );
+
+    const marketUpdateProposer = await deploymentManager.deploy(
+      'marketUpdateProposer',
+      'marketupdates/MarketUpdateProposer.sol',
+      [
+        governorTimelockAddress,
+        marketUpdateMultiSig,
+        marketUpdateProposalGuardian,
+        marketUpdateTimelock.address,
+      ],
+      true
+    );
+
+    const configurator = await deploymentManager.deploy(
+      'configuratorNew',
+      'Configurator.sol',
+      [],
+      true
+    );
+
+    const cometProxyAdmin = await deploymentManager.deploy(
+      'cometProxyAdminNew',
+      'CometProxyAdmin.sol',
+      [governorTimelockAddress],
+      true
+    );
+
+    const marketAdminPermissionChecker = await deploymentManager.deploy(
+      'marketAdminPermissionChecker',
+      'marketupdaes/MarketAdminPermissionChecker',
+      [
+        governorTimelockAddress,
+        ethers.constants.AddressZero,
+        ethers.constants.AddressZero,
+      ]
+    );
+    const deployedContracts = {
+      marketUpdateTimelock,
+      marketUpdateProposer,
+      configurator,
+      cometProxyAdmin,
+      marketAdminPermissionChecker,
+    };
+
     return { deployedContracts };
   },
 
@@ -41,45 +97,45 @@ export default migration("1728038985_my_migration", {
     const cometProxyAddress = addresses.markets[0].cometProxyAddress;
 
     const changeProxyAdminForCometProxyCalldata = utils.defaultAbiCoder.encode(
-      ["address", "address"],
+      ['address', 'address'],
       [cometProxyAddress, cometProxyAdminNewAddress]
     );
 
     const changeProxyAdminForConfiguratorProxyCalldata =
       utils.defaultAbiCoder.encode(
-        ["address", "address"],
+        ['address', 'address'],
         [configuratorProxyAddress, cometProxyAdminNewAddress]
       );
 
     const upgradeConfiguratorProxyCalldata = utils.defaultAbiCoder.encode(
-      ["address", "address"],
+      ['address', 'address'],
       [configuratorProxyAddress, configuratorNewAddress]
     );
 
     const setMarketAdminCalldata = utils.defaultAbiCoder.encode(
-      ["address"],
+      ['address'],
       [marketUpdateTimelockAddress]
     );
 
     const setMarketAdminPermissionCheckerForConfiguratorProxyCalldata =
       utils.defaultAbiCoder.encode(
-        ["address"],
+        ['address'],
         [marketAdminPermissionCheckerAddress]
       );
 
     const setMarketAdminPermissionCheckerForCometProxyCalldata =
       utils.defaultAbiCoder.encode(
-        ["address"],
+        ['address'],
         [marketAdminPermissionCheckerAddress]
       );
 
     const setMarketUpdateProposerCalldata = utils.defaultAbiCoder.encode(
-      ["address"],
+      ['address'],
       [marketUpdateProposerAddress]
     );
 
     const l2ProposalData = utils.defaultAbiCoder.encode(
-      ["address[]", "uint256[]", "string[]", "bytes[]"],
+      ['address[]', 'uint256[]', 'string[]', 'bytes[]'],
       [
         [
           cometProxyAdminOldAddress,
@@ -92,13 +148,13 @@ export default migration("1728038985_my_migration", {
         ],
         [0, 0, 0, 0, 0, 0, 0],
         [
-          "changeProxyAdmin(address,address)",
-          "changeProxyAdmin(address,address)",
-          "upgrade(address,address)",
-          "setMarketAdmin(address)",
-          "setMarketAdminPermissionChecker(address)",
-          "setMarketAdminPermissionChecker(address)",
-          "setMarketUpdateProposer(address)",
+          'changeProxyAdmin(address,address)',
+          'changeProxyAdmin(address,address)',
+          'upgrade(address,address)',
+          'setMarketAdmin(address)',
+          'setMarketAdminPermissionChecker(address)',
+          'setMarketAdminPermissionChecker(address)',
+          'setMarketUpdateProposer(address)',
         ],
         [
           changeProxyAdminForCometProxyCalldata,
@@ -115,19 +171,19 @@ export default migration("1728038985_my_migration", {
     const actions = [
       {
         contract: scrollMessenger,
-        signature: "sendMessage(address,uint256,bytes,uint256)",
+        signature: 'sendMessage(address,uint256,bytes,uint256)',
         args: [bridgeReceiver.address, 0, l2ProposalData, 600_000],
         value: exp(0.1, 18),
       },
     ];
 
     const description =
-      "Governance proposal with actions to change proxy admins, upgrade the configurator, and set the market admin and related roles.";
+      'Governance proposal with actions to change proxy admins, upgrade the configurator, and set the market admin and related roles.';
     const txn = await govDeploymentManager.retry(async () =>
       trace(await governor.propose(...(await proposal(actions, description))))
     );
 
-    const event = txn.events.find((event) => event.event === "ProposalCreated");
+    const event = txn.events.find((event) => event.event === 'ProposalCreated');
     const [proposalId] = event.args;
 
     trace(`Created proposal ${proposalId}.`);

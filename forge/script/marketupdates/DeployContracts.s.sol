70bce05e867552afebbdcc43895d4e37131c97d5
MSamiTariq <samitariq40@gmail.com>
2024-10-08 23:42:45 +0500

    moved library code to script

--- forge/script/marketupdates/DeployContracts.s.sol
+++ forge/script/marketupdates/DeployContracts.s.sol
@@ -10,10 +10,19 @@ import "@comet-contracts/CometProxyAdmin.sol";
 import "@comet-contracts/marketupdates/MarketAdminPermissionChecker.sol";
 import "@comet-contracts/Create2DeployerInterface.sol";
 import "./helpers/MarketUpdateAddresses.sol";
-import "./helpers/MarketUpdateContractsDeployer.sol";
 
 contract DeployContracts is Script {
     address public deployedWalletAddress;
+
+    address constant public create2DeployerAddress = 0x13b0D85CcB8bf860b6b79AF3029fCA081AE9beF2;
+
+    struct DeployedContracts {
+        address marketUpdateTimelock;
+        address marketUpdateProposer;
+        address newCometProxyAdmin;
+        address newConfiguratorImplementation;
+        address marketAdminPermissionChecker;
+    }
 
     struct ContractDeploymentParams {
         bytes creationCode;
@@ -27,10 +36,10 @@ contract DeployContracts is Script {
 
         vm.startBroadcast(vm.envUint("PRIVATE_KEY"));
         
-        bytes32 salt = keccak256(abi.encodePacked("Salt-31")); 
+        bytes32 salt = keccak256(abi.encodePacked("Salt-34")); 
 
         /// Call library function
-        MarketUpdateContractsDeployer.DeployedContracts memory deployedContracts = MarketUpdateContractsDeployer.deployContracts(
+        DeployedContracts memory deployedContracts = deployContracts(
             salt,
             MarketUpdateAddresses.MARKET_UPDATE_MULTISIG_ADDRESS,
             MarketUpdateAddresses.MARKET_ADMIN_PAUSE_GUARDIAN_ADDRESS,
@@ -46,5 +55,148 @@ contract DeployContracts is Script {
         console.log("MarketAdminPermissionChecker: ", deployedContracts.marketAdminPermissionChecker);
 
         vm.stopBroadcast();
+    }
+
+    function deployContracts(
+        bytes32 salt,
+        address marketUpdateMultiSig, // TODO: Check this is properly used
+        address marketAdminPauseGuardianAddress, // TODO: Check this is properly used
+        address marketUpdateProposalGuardianAddress, // TODO: Check this is properly used
+        address governorTimelockAddress
+    ) public returns (DeployedContracts memory) {
+
+        ICreate2Deployer create2Deployer = ICreate2Deployer(create2DeployerAddress);
+
+        // Prepare deployment parameters for each contract
+        ContractDeploymentParams memory marketUpdateTimelockParams = ContractDeploymentParams({
+            creationCode: type(MarketUpdateTimelock).creationCode,
+            constructorArgs: abi.encode(governorTimelockAddress, 360000), // TODO: add comment on how 360000 is calculated
+            expectedRuntimeCode: type(MarketUpdateTimelock).runtimeCode,
+            contractName: "MarketUpdateTimelock"
+        });
+
+        address computedMarketUpdateTimelockAddress = deployContractWithCreate2(create2Deployer, salt, marketUpdateTimelockParams);
+
+        ContractDeploymentParams memory marketUpdateProposerParams = ContractDeploymentParams({
+            creationCode: type(MarketUpdateProposer).creationCode,
+            constructorArgs: abi.encode(
+                governorTimelockAddress,
+                marketUpdateMultiSig,
+                marketUpdateProposalGuardianAddress,
+                computedMarketUpdateTimelockAddress
+            ),
+            expectedRuntimeCode: type(MarketUpdateProposer).runtimeCode,
+            contractName: "MarketUpdateProposer"
+        });
+
+        address computedMarketUpdateProposerAddress = deployContractWithCreate2(create2Deployer, salt, marketUpdateProposerParams);
+
+        ContractDeploymentParams memory configuratorParams = ContractDeploymentParams({
+            creationCode: type(Configurator).creationCode,
+            constructorArgs: "",
+            expectedRuntimeCode: type(Configurator).runtimeCode,
+            contractName: "Configurator"
+        });
+
+        address computedConfiguratorAddress = deployContractWithCreate2(create2Deployer, salt, configuratorParams);
+
+        ContractDeploymentParams memory cometProxyAdminParams = ContractDeploymentParams({
+            creationCode: type(CometProxyAdmin).creationCode,
+            constructorArgs: abi.encode(governorTimelockAddress),
+            expectedRuntimeCode: type(CometProxyAdmin).runtimeCode,
+            contractName: "CometProxyAdmin"
+        });
+
+        address computedCometProxyAdminAddress = deployContractWithCreate2(create2Deployer, salt, cometProxyAdminParams);
+
+        console.log("Owner of cometProxyAdmin: ", CometProxyAdmin(computedCometProxyAdminAddress).owner());
+
+        ContractDeploymentParams memory marketAdminPermissionCheckerParams = ContractDeploymentParams({
+            creationCode: type(MarketAdminPermissionChecker).creationCode,
+            constructorArgs: abi.encode(governorTimelockAddress, address(0), address(0)),
+            expectedRuntimeCode: type(MarketAdminPermissionChecker).runtimeCode,
+            contractName: "MarketAdminPermissionChecker"
+        });
+
+        address computedMarketAdminPermissionCheckerAddress = deployContractWithCreate2(create2Deployer, salt, marketAdminPermissionCheckerParams);
+
+        return DeployedContracts({
+            marketUpdateTimelock: computedMarketUpdateTimelockAddress,
+            marketUpdateProposer: computedMarketUpdateProposerAddress,
+            newCometProxyAdmin: computedCometProxyAdminAddress,
+            newConfiguratorImplementation: computedConfiguratorAddress,
+            marketAdminPermissionChecker: computedMarketAdminPermissionCheckerAddress
+        });
+    }
+
+    function deployContractWithCreate2(
+        ICreate2Deployer create2Deployer,
+        bytes32 salt,
+        ContractDeploymentParams memory params
+    ) internal returns (address) {
+        bytes memory bytecode = abi.encodePacked(params.creationCode, params.constructorArgs);
+        address computedAddress = create2Deployer.computeAddress(salt, keccak256(bytecode));
+        checkOrDeployAndCompareBytecodes(create2Deployer, salt, bytecode, computedAddress, params.expectedRuntimeCode);
+        return computedAddress;
+    }
+
+    function checkOrDeployAndCompareBytecodes(
+        ICreate2Deployer create2Deployer,
+        bytes32 salt,
+        bytes memory actualBytecode,
+        address computedAddress,
+        bytes memory expectedBytecode
+    ) internal {
+        uint256 contractCodeSize = getContractCodeSizeAtAddress(computedAddress);
+
+        if (contractCodeSize > 0) {
+            bytes memory deployedBytecode = verifyDeployedBytecode(computedAddress, contractCodeSize);
+
+            require(
+                keccak256(deployedBytecode) == keccak256(expectedBytecode),
+                "Deployed bytecode does not match the expected bytecode"
+            );
+        } else {
+            deployAndCompareBytecodes(create2Deployer, salt, actualBytecode, computedAddress, expectedBytecode);
+        }
+    }
+
+    function deployAndCompareBytecodes(
+        ICreate2Deployer create2Deployer,
+        bytes32 salt,
+        bytes memory actualBytecode,
+        address computedAddress,
+        bytes memory expectedBytecode
+    ) internal {
+        create2Deployer.deploy(0, salt, actualBytecode);
+
+        uint256 size = getContractCodeSizeAtAddress(computedAddress);
+        require(size > 0, "No contract deployed at this address");
+
+        bytes memory deployedBytecode = new bytes(size);
+        assembly {
+            extcodecopy(computedAddress, add(deployedBytecode, 0x20), 0, size)
+        }
+
+        require(
+            keccak256(deployedBytecode) == keccak256(expectedBytecode),
+            "Deployed bytecode does not match the expected bytecode"
+        );
+    }
+
+    function getContractCodeSizeAtAddress(address contractAddress) internal view returns (uint256) {
+        uint256 size;
+        assembly {
+            size := extcodesize(contractAddress)
+        }
+        return size;
+    }
+
+    function verifyDeployedBytecode(address computedAddress, uint256 contractCodeSize) internal view returns (bytes memory) {
+        bytes memory deployedBytecode = new bytes(contractCodeSize);
+        assembly {
+            extcodecopy(computedAddress, add(deployedBytecode, 0x20), 0, contractCodeSize)
+        }
+        return deployedBytecode;
     }
 }

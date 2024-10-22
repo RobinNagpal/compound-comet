38ce605d08022ca7b41b7a79317871437ad3184f
robin <robinnagpal.tiet@gmail.com>
2024-10-22 11:02:27 -0400

    Add log statement and returns to debug scenarios

--- scenario/utils/index.ts
+++ scenario/utils/index.ts
@@ -8,7 +8,7 @@ import CometAsset from '../context/CometAsset';
 import { exp } from '../../test/helpers';
 import { DeploymentManager } from '../../plugins/deployment_manager';
 import { impersonateAddress } from '../../plugins/scenario/utils';
-import { ProposalState, OpenProposal } from '../context/Gov';
+import {ProposalState, OpenProposal, IGovernorBravo} from '../context/Gov';
 import { debug } from '../../plugins/deployment_manager/Utils';
 import { COMP_WHALES } from '../../src/deploy';
 import relayMessage from './relayMessage';
@@ -361,10 +361,14 @@ export async function executeOpenProposal(
   dm: DeploymentManager,
   { id, startBlock, endBlock }: OpenProposal
 ) {
-  const governor = await dm.getContractOrThrow('governor');
+
+
+  const governor = await dm.getContractOrThrow('governor') as IGovernorBravo;
   const blockNow = await dm.hre.ethers.provider.getBlockNumber();
   const blocksUntilStart = startBlock.toNumber() - blockNow;
   const blocksUntilEnd = endBlock.toNumber() - Math.max(startBlock.toNumber(), blockNow);
+
+  debug(`Proposal state ${id} - ${await governor.state(id)}`);
 
   if (blocksUntilStart > 0) {
     await mineBlocks(dm, blocksUntilStart);
@@ -373,6 +377,7 @@ export async function executeOpenProposal(
   const compWhales = dm.network === 'mainnet' ? COMP_WHALES.mainnet : COMP_WHALES.testnet;
 
   if (blocksUntilEnd > 0) {
+    debug(`Emulating Voting for proposal ${id}`);
     for (const whale of compWhales) {
       try {
         // Voting can fail if voter has already voted
@@ -394,11 +399,15 @@ export async function executeOpenProposal(
 
   // Execute proposal (maybe, w/ gas limit so we see if exec reverts, not a gas estimation error)
   if (await governor.state(id) == ProposalState.Queued) {
+    debug(`Executing proposal ${id} on network ${dm.network}`);
     const block = await dm.hre.ethers.provider.getBlock('latest');
     const proposal = await governor.proposals(id);
     await setNextBlockTimestamp(dm, Math.max(block.timestamp, proposal.eta.toNumber()) + 1);
     await setNextBaseFeeToZero(dm);
-    await governor.execute(id, { gasPrice: 0, gasLimit: 12000000 });
+    if(dm.network === 'mainnet') {
+      return;
+    }
+    const result = await governor.execute(id, { gasPrice: 0, gasLimit: 12000000 });
   }
   await redeployRenzoOracle(dm);
 }
@@ -412,7 +421,7 @@ export async function fastGovernanceExecute(
   signatures: string[],
   calldatas: string[]
 ) {
-  const governor = await dm.getContractOrThrow('governor');
+  const governor = await dm.getContractOrThrow('governor') as IGovernorBravo;
 
   await setNextBaseFeeToZero(dm);
 
